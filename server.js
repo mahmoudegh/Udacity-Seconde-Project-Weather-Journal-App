16e9447f0d45e20740ee479ba1f0f02966be05d7
mahmoudegh <48510315+mahmoudegh@users.noreply.github.com>
2021-05-08 15:11:30 +0200

    Start Project

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
+
+/* Dependencies */
+const bodyParser = require("body-parser");
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,8 +16,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require("cors");
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 // Setup Server
+const port = 8000;
+
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log("server running");
+  console.log(`running on localhost: ${port}`);
+}
+
+// Arrow Function
+// const server = app.listen(port, () => {
+//   console.log(`running on localhost: ${port}`);
+// });
+
+// respond with "projectData" when a GET request is made to the homepage
+app.get("/add", function (req, res) {
+  res.send(projectData);
+});
+
+// setup a basic POST route in the server side code
+// First, Create an array to hold data
+const data = [];
+
+// Then, create post() with a url path and a callback function
+app.post("/add", function (req, res) {
+  // res.send("POST received");
+  console.log(req.body);
+  data.push(req.body);
+  console.log(data);
+});
+
+// Arrow function
+// app.get("/", (request, response) => {
+//   response.send(projectData);
+// });
+
+// let data = request.body;
+// projectData["intelligence"] = data.intelligence;
+projectData.push(request.body);
--- website/app.js
+++ website/app.js
@@ -1,5 +1,36 @@
+// const openWeatherMap = a26e439037a7723ccab5cff83466280d;
+
+const postData = async (url = "", data = {}) => {
+  console.log(data);
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    // Body data type must match "Content-Type" header
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+postData("/add", { answer: 42, score: 5 });
+postData("/add", { answer: 100, score: 6 });
+
+////////////////////////////////////////////////////////////////////////////////////////
+
 /* Global Variables */
+// const openWeatherMap = a26e439037a7723ccab5cff83466280d;
+
+// openWeatherMap.fetch();
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();

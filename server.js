59b8a66b44cb5ba2c44f108561da6d2fe1f741e5
GalalNana <mahmoudegh@yahoo.com>
2021-05-11 01:36:19 +0200

    Test

--- server.js
+++ server.js
@@ -1,64 +1,39 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
+/* Empty JS object to act as endpoint for all routes */
+const projectData = {};
 
-// Require Express to run server and routes
-const express = require("express");
+/* Express to run server and routes */
+const express = require('express');
 
-// Start up an instance of app
+/* Start up an instance of app */
 const app = express();
 
 /* Dependencies */
-const bodyParser = require("body-parser");
-
+const bodyParser = require('body-parser');
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-const cors = require("cors");
+const cors = require('cors');
 app.use(cors());
 
-// Initialize the main project folder
-app.use(express.static("website"));
+/* Initialize the main project folder*/
+app.use(express.static('website'));
 
-// Setup Server
-const port = 8000;
+const port = 3000;
 
-const server = app.listen(port, listening);
-
-function listening() {
-  console.log("server running");
+/* Spin up the server*/
+const server = app.listen(port, () => {
   console.log(`running on localhost: ${port}`);
-}
+});
 
-// Arrow Function Method
-// const server = app.listen(port, () => {
-//   console.log(`running on localhost: ${port}`);
-// });
-
-// respond with "projectData" when a GET request is made to the homepage
-app.get("/add", function (req, res) {
+// GET route => Respond with JS object when a GET request is made to the homepage
+app.get('/', (req, res) => {
   res.send(projectData);
 });
 
-// setup a basic POST route in the server side code
-// First, Create an array to hold data
+// POST route
 const data = [];
 
-// Then, create post() with a url path and a callback function
-app.post("/add", function (req, res) {
-  // res.send("POST received");
-  console.log(req.body);
+app.post('/', (req, res) => {
   data.push(req.body);
-  console.log(data);
+  res.send(projectData);
 });
-
-// Arrow function
-// app.get("/", (request, response) => {
-//   response.send(projectData);
-// });
-
-// let data = request.body;
-// projectData["intelligence"] = data.intelligence;
-// projectData.push(request.body);
--- website/app.js
+++ website/app.js
@@ -1,36 +1,36 @@
-// const openWeatherMap = a26e439037a7723ccab5cff83466280d;
+const baseURL = 'http://api.openweathermap.org/data/2.5/forecast?id=';
+const apiKey = '&appid=a26e439037a7723ccab5cff83466280d';
+const zipCode = document.getElementById('zip');
+const date = document.getElementById('date');
+const temp = document.getElementById('temp');
+const content = document.getElementById('content');
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = `${d.getDate()} / ${d.getMonth() + 1} / ${d.getFullYear()}`;
+date.innerHTML = `Date : ${newDate}`;
 
-const postData = async (url = "", data = {}) => {
-  console.log(data);
-  const response = await fetch(url, {
-    method: "POST",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    // Body data type must match "Content-Type" header
-    body: JSON.stringify(data),
-  });
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+  postData(baseURL, zipCode.value, apiKey);
+}
+/* Function to GET data */
+const postData = async (url, zip, key) => {
+  const response = await fetch(url + zip + key);
 
   try {
     const newData = await response.json();
+    console.log(newData.list[0].main.temp);
     console.log(newData);
+    // Object.entries(newData.list).forEach((el) => {
+    //   console.log(el[1].main.temp);
+    // });
+    // console.log(Object.entries(newData)[3]);
     return newData;
   } catch (error) {
-    console.log("error", error);
+    console.log('error', error);
+    // appropriately handle the error
   }
 };
 
-postData("/add", { answer: 42, score: 5 });
-postData("/add", { answer: 100, score: 6 });
-
-////////////////////////////////////////////////////////////////////////////////////////
-
-/* Global Variables */
-// const openWeatherMap = a26e439037a7723ccab5cff83466280d;
-
-// openWeatherMap.fetch();
-
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+// TO DO-Call Function
